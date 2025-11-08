60ec20d0baea8896b4d383910937fd9cd957cf27
theskylla <theskylla@gmail.com>
2025-11-08 23:44:45 +0100

    added shading under overlay, instead of box-shading

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ function showNav() {
     let overlay = document.getElementById("overlay-menu");
 
     if (!overlay) {
+        shading = document.createElement("div");
+        shading.className = "shading";
+
+        
+
         overlay = document.createElement("div");
         overlay.id = "overlay-menu";
         overlay.classList.add("overlay");
@@ -15,14 +20,17 @@ function showNav() {
         close.className = "fa-solid fa-x";
         close.classList.add("close");
        
-        close.onclick = () => overlay.remove(); 
+        close.onclick = () => {
+        overlay.remove(), shading.remove(); 
+    };
         overlay.appendChild(close);
-
+      
         navbarItems.forEach(li => {
             const liDupe = li.cloneNode(true); 
             newList.appendChild(liDupe);
         });
 
+        document.body.appendChild(shading);
         overlay.appendChild(newList);
 
         document.body.appendChild(overlay);
@@ -30,5 +38,7 @@ function showNav() {
     } else {
         // If it exists, remove it (toggle behavior)
         overlay.remove();
+        shading.remove();
+    
     }
 }
